6b50d7efffbf6369ea7fd12268a2b07261fe47b5
ramanandsagard-beep <ramanandsagard@gmail.com>
2026-02-25 16:48:00 +0530

    UC11:pattern

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,57 @@
-public class PalindromeChecker {
+interface PalindromeStrategy {
+    boolean isPalindrome(String input);
+}
 
-    public boolean checkPalindrome(String input) {
-        if (input == null) return false;
+// Stack Strategy
+class StackStrategy implements PalindromeStrategy {
+    public boolean isPalindrome(String input) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+        for (char c : input.toCharArray()) stack.push(c);
 
-        String normalized = input.toLowerCase().replaceAll("\\s+", "");
-        int left = 0, right = normalized.length() - 1;
-
-        while (left < right) {
-            if (normalized.charAt(left) != normalized.charAt(right))
-                return false;
-            left++;
-            right--;
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) return false;
         }
         return true;
     }
+}
 
+// Deque Strategy
+class DequeStrategy implements PalindromeStrategy {
+    public boolean isPalindrome(String input) {
+        java.util.Deque<Character> deque = new java.util.LinkedList<>();
+        for (char c : input.toCharArray()) deque.addLast(c);
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast())
+                return false;
+        }
+        return true;
+    }
+}
+
+// Context Class
+class PalindromeContext {
+    private PalindromeStrategy strategy;
+
+    public PalindromeContext(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void setStrategy(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean execute(String input) {
+        return strategy.isPalindrome(input);
+    }
+}
+
+public class UC12_StrategyDemo {
     public static void main(String[] args) {
-        PalindromeChecker checker = new PalindromeChecker();
-        System.out.println(checker.checkPalindrome("Madam")); // true
+        PalindromeContext context = new PalindromeContext(new StackStrategy());
+        System.out.println(context.execute("level"));
+
+        context.setStrategy(new DequeStrategy());
+        System.out.println(context.execute("hello"));
     }
 }
