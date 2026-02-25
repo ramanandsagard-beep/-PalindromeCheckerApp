ca09c18ef36469c756f220a2e63637e5052c8607
ramanandsagard-beep <ramanandsagard@gmail.com>
2026-02-25 16:42:28 +0530

    UC9:recursive

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,67 +1,20 @@
-public class UC8_LinkedListPalindrome {
-
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-        }
-    }
+public class UC9_RecursivePalindrome {
 
     public static boolean isPalindrome(String input) {
-        if (input == null || input.length() <= 1) return true;
-
-        Node head = buildList(input);
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverse(slow);
-        Node copySecondHalf = secondHalf;
-
-        Node firstHalf = head;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data)
-                return false;
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        reverse(copySecondHalf); // restore list (optional)
-        return true;
+        return check(input, 0, input.length() - 1);
     }
 
-    private static Node buildList(String input) {
-        Node head = new Node(input.charAt(0));
-        Node current = head;
+    private static boolean check(String str, int left, int right) {
+        if (left >= right) return true;  // Base condition
 
-        for (int i = 1; i < input.length(); i++) {
-            current.next = new Node(input.charAt(i));
-            current = current.next;
-        }
-        return head;
-    }
+        if (str.charAt(left) != str.charAt(right))
+            return false;
 
-    private static Node reverse(Node head) {
-        Node prev = null;
-        while (head != null) {
-            Node next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
-        }
-        return prev;
+        return check(str, left + 1, right - 1);
     }
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("radar")); // true
-        System.out.println(isPalindrome("java"));  // false
+        System.out.println(isPalindrome("noon"));  // true
+        System.out.println(isPalindrome("apple")); // false
     }
 }
